a4c09c61e9c16e3913df6bccceac426ac5d1bfc0
Galdormin <theo.plenet@gmail.com>
2025-02-10 11:53:05 +0100

    feat(tools): Add hot reload to /app

--- pocketbase/app/vite.config.ts
+++ pocketbase/app/vite.config.ts
@@ -2,9 +2,15 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-    plugins: [sveltekit()],
-    server: {
-        port: 3000,
-        host: true,
-    }
+  plugins: [sveltekit()],
+  server: {
+    port: 3000,
+    host: true,
+    watch: {
+      usePolling: true,
+    },
+    hmr: {
+      host: 'localhost',
+    },
+  },
 });
